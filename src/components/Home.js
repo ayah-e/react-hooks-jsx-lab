9d1ceb8d1976ffb673eff1749735686426b1835c
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-01 11:35:13 -0500

    added elements to About

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+//this works but we will us another method
+// const h1Style = {
+//   color: "firebrick"
+// };
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style ={{color: "firebrick"}}>{`${name} is a Web Developer from Your ${city}`}</h1>
+    </div>
+  );
 }
 
 export default Home;
